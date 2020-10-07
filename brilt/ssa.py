382a2db8950c2f32643a6a16227dc1c524cad214
tedbauer <tjb272@cornell.edu>
2020-10-07 11:33:15 -0400

    Fix phi argument renaming bug

--- brilt/ssa.py
+++ brilt/ssa.py
@@ -5,18 +5,19 @@ import json
 
 
 def gen_fresh_name(prefix, blocks):
-    c = 1
-    name = prefix + "." + str(c)
-    found_new_name = False
-    while not found_new_name:
+    c = 0
+    name = None
+    while True:
+        c += 1
+        name = prefix + "." + str(c)
+        found_new_name = True
         for block in blocks:
             for instr in block:
                 if "dest" in instr:
                     if name == instr["dest"]:
-                        c += 1
-                        name = prefix + "." + str(c)
-                    continue
-        found_new_name = True
+                        found_new_name = False
+        if found_new_name: break
+    assert name is not None
     return name
 
 
@@ -26,7 +27,7 @@ def rename(bname, block, blocks, stack, dom_tree, cfg, name2block, varz):
     for instr in block:
         if "args" in instr:
             for i, arg in enumerate(instr["args"]):
-                if arg != "cond":
+                if arg != "cond" and instr["op"] != "phi":
                     instr["args"][i] = stack[arg][-1]
 
         if "dest" in instr:
@@ -37,6 +38,11 @@ def rename(bname, block, blocks, stack, dom_tree, cfg, name2block, varz):
             stack[old_name].append(new_name)
 
     # do phi node stuff here
+    for s in cfg[bname]:
+        for instr in name2block[s]:
+            if "op" in instr and instr["op"] == "phi":
+                instr["args"].append(stack[instr["orig_name"]][-1])
+                instr["labels"].append(bname)
 
     for b in dom_tree[bname]:
         rename(b, name2block[b], blocks, stack, dom_tree, cfg, name2block, varz)
@@ -64,19 +70,30 @@ def to_ssa(prog):
     for v in varz:
         for d in defs[v]:
             for bname in gen_dom_frontier(cfg, d):
-                phi_node = {
-                    "args": [v, v],
-                    "dest": v,
-                    "labels": ["left", "right"],
-                    "op": "phi",
-                    "type": "int",
-                }
-                # todo: if no label in block, insert at pos 1
-                #name2block[bname].insert(1, phi_node)
+                has_phi = False
+                for instr in name2block[bname]:
+                    if "dest" in instr and instr["dest"] == v and instr["op"] == "phi":
+                        has_phi = True
+                if not has_phi:
+                    phi_node = {
+                        "args": [],
+                        "dest": v,
+                        "labels": [],
+                        "op": "phi",
+                        "type": "int",
+                        "orig_name": v
+                    }
+                    # todo: if no label in block, insert at pos 1
+                    name2block[bname].insert(1, phi_node)
 
     stack = dict()
     for v in varz: stack[v] = []
     rename("entry", name2block["entry"], blocks, stack, dom_tree, cfg, name2block, varz)
+
+    for bname in name2block:
+        for instr in name2block[bname]:
+            if "phi" in instr:
+                del name2block[bname]["orig_name"]
 
     new_prog = {
         "functions": [{
